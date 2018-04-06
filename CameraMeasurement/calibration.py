73c92eeb1e0bc09c6a6907fafee2698a55a33076
lorenz-lukas <lukaslorenzdeandrade@gmail.com>
2018-04-06 08:49:21 -0300

    draw

--- CameraMeasurement/calibration.py
+++ CameraMeasurement/calibration.py
@@ -6,8 +6,12 @@ import glob
 import math
 import sys
 
-# termination criteria
+video = cv2.VideoCapture(0)
+ret, frame = video.read()
+pic = cv2.flip(frame,1)
+
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*7,3), np.float32)
 objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
@@ -15,6 +19,7 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 images = glob.glob('*.jpg')
+
 for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
--- CameraMeasurement/main.py
+++ CameraMeasurement/main.py
@@ -10,42 +10,24 @@ import sys
 video = cv2.VideoCapture(0) # takes the video adress
 (ret, pic) = video.read()
 np.seterr(over='ignore')
-position = 0
-#position = []
-#position.append(1)
-#position.append(1)
-flag = 0
+px = []
+click = 0
 def f(x):
 	pass
 
 def coordinates(event,x,y,flags,param):
-	global flag,position
-	(height, width) = pic.shape[:2]
-
+	global px, click
 	if(event == cv2.EVENT_LBUTTONDBLCLK):
-		px = pic[y,x]
+		px.append((x,y))
 		print('Press ESC to exit!\n')
 		print 'Coordinates:',(x,y)
-		print 'BGR:',px
-		flag = 1
-	if(flag):
-		px = pic[y,x]
-		print(px)
-		for i in range(width):
-			for j in range(height):
-				px2 = pic[j,i]
-				if(position == 0):
-					distance = np.sqrt(((px[0]-px2[0])**2) + ((px[1]-px2[1])**2)+((px[2]-px2[2])**2))
-					if(distance < 13):
-						pic[j,i] = [0,0,255]
-				else:
-					distance = (px-px2)
-					if(distance < 13):
-						pic[j,i] = 0
-		flag = 0
-def open():
-	global video, pic,ret,position
-	#cap = cv2.VideoCapture(0)
+		print 'BGR:',px[-1]
+		print('Selecione mais um ponto')
+		click = click + 1
+
+def draw_line():
+	global video, pic,ret,click
+	distance  = 0
 	key = 0
 	cv2.namedWindow('Choose a color by a double click')
 	cv2.setMouseCallback('Choose a color by a double click',coordinates)
@@ -58,12 +40,23 @@ def open():
 		position = cv2.getTrackbarPos(switch,'Choose a color by a double click')
 		if(position!=0):
 			pic = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+		if(click==2):
+			click = 0
+			distance = np.sqrt((px[-1][-1]-px[-2][-1])**2 + ((px[-1][-2]-px[-2][-2])**2)) # sqrt(dy²+dx²)
+			print "\nPixel distance:", distance, "\n"
+		if(distance!=0):
+			cv2.line(pic,(px[-1][-2],px[-1][-1]),(px[-2][-2],px[-2][-1]),(0,0,255),5)
+			#font = cv2.FONT_HERSHEY_SIMPLEX
+			#cv2.putText(pic,'Distance:',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
 		cv2.imshow('Choose a color by a double click', pic)
 		key = cv2.waitKey(20)
 	video.release()
 	cv2.destroyAllWindows()
+def calibration():
+	print("oi")
 
 def main():
-	open()
+	draw_line()
+	calibration()
 	video.release()
 main()
