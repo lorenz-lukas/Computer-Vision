c9f49eeabffd2b38cb712a1c25e8487588f8b38e
lorenz-lukas <lukaslorenzandrade@gmail.com>
2018-04-12 14:44:14 -0300

    Classifiers program

--- classification/dataset.m
+++ classification/dataset.m
@@ -8,12 +8,13 @@ img = imread('photo-9-orig.jpg');
 img = imresize(img,6);
 figure, imshow(img)
 for i = 1:100 
-    [x,y] = ginput(1);s
+    [x,y] = ginput(1);
     px = img(int16(x),int16(y));
     label = input('Set the pixel label according to:\n1.Green leaves; 2. Ground;\n3. Yellow and red leaves and fruits; 4. Shadows or unknown.\n')
     data(1,i) = px;
     data(2,i) = label;
 end
 xlswrite('dataset.xlsx',data);
+close all
 %dados = xlsread('pendulo_trifilar_pequeno_ugollini.xlsx');
 %tempo1 = dados(1:3323,1); % 1:3323
