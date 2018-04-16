c85f8b908f1e514dd8bddc740468a3f79256e4e2
gustavocsousa <gustavo_csousa@hotmail.com>
2018-04-16 17:34:42 -0300

    Update dataset.m

--- classification/dataset.m
+++ classification/dataset.m
@@ -6,13 +6,16 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 img = imread('photo-9-orig.jpg');
 img = imresize(img,6);
-figure, imshow(img)
+figure, imshow(img);
 for i = 1:100 
-    [x,y] = ginput(1);
-    px = img(int16(x),int16(y));
+    [y,x] = ginput(1);
+    px = img(int16(x),int16(y),:);
+    fprintf('%d input\n\n', i);
     label = input('Set the pixel label according to:\n1.Green leaves; 2. Ground;\n3. Yellow and red leaves and fruits; 4. Shadows or unknown.\n')
-    data(1,i) = px;
-    data(2,i) = label;
+    data(i,1) = px(:,:,1);
+    data(i,2) = px(:,:,2);
+    data(i,3) = px(:,:,3);
+    data(i,4) = label;
 end
 xlswrite('dataset.xlsx',data);
 close all
